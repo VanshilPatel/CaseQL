715ade74fa908b9dfad3010d7b4734a56540a05a
Vanshil Patel <vanshilpatel12@gmail.com>
2024-03-26 17:13:43 +0530

    Complete Query Acceptance and Rejection logic

--- frontend/src/components/Brief.jsx
+++ frontend/src/components/Brief.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Brief = () => {
-
+   
+   const [brief, setBrief] = useState();
    const navigate = useNavigate();
+
+   var config = {
+    method: 'get',
+    url: 'http://localhost:5342/brief/5',
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+  };
+  
+  axios(config)
+  .then(function (response) {
+   setBrief(response.data.description)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
 
    const handleClick = ()=>{
     navigate('/query')
@@ -13,7 +32,7 @@ const Brief = () => {
      return (
     <div className="flex flex-col items-center min-h-screen gap-y-4 mt-44"> 
     <h1 className='font-extrabold text-2xl'>Mission Brief</h1>
-    <p className='max-w-[600px] font-semibold '>All illegal site's servers were seized in a recent operation. Please submit all the users' details</p>
+    <p className='max-w-[600px] font-semibold '>{brief}</p>
     <h2 className='mt-7'>Use SQL to retrieve the data and submit the resulting table</h2>
     <button className='border rounded-md bg-slate-500 p-3' onClick={handleClick}>Continue</button>
   </div>
--- frontend/src/components/Query.jsx
+++ frontend/src/components/Query.jsx
@@ -1,13 +1,58 @@
 import { Delete } from 'lucide-react';
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 const Query = () => {
   const symbols = ["*", ">", "<", "=", "!=", "NOT", "AND", "OR", "DISTINCT", "LIMIT", "IN"];
   const keywords = ["SELECT", "WHERE", "FROM", "GROUP", "BY"];
-  const keywords2 = ["firstName", "lastName", "emailAdd", "posts", "lastAccess"];  
-
+  const keywords2 = ["firstName", "lastName", "emailAdd", "posts", "answer"];
+  const numbers = ["0","1","2","3","4","5","6","7","8","9","0",] 
+  const [id, setId] = useState(5); 
   const [value, setValue] = useState(' ');
+  const [brief, setBrief] = useState();
+  const navigate = useNavigate();
+
+  const requestBody = {
+    id: id.toString(), 
+    value: value.toString(),
+  };
+
+  const executeQuery = ()=>{
+
+    axios.post('http://localhost:5342/query', requestBody)
+      .then(function (response) {
+
+        if(response.data.message == "success"){
+          navigate('/results')
+        }
+        navigate('/results')
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  }
+   
+
+  var config = {
+    method: 'get',
+    url: 'http://localhost:5342/brief/5',
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+  };
+  
+  axios(config)
+  .then(function (response) {
+   setBrief(response.data.description)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
 
   const handleClick = (buttonName) => {
     setValue((prevValue) => prevValue ? `${prevValue} ${buttonName}` : buttonName);
@@ -35,7 +80,7 @@ const Query = () => {
 
       <div className='flex mt-3 ml-96 space-x-3'>
         <button className='bg-slate-950 text-gray-50 border rounded-md p-3 w-1/12 mr-3'  onClick={() => handleClick(';')}>;</button> 
-        <button className='bg-slate-950 text-gray-50 border rounded-md p-3 w-1/12'>Execute</button>
+        <button onClick={executeQuery} className='bg-slate-950 text-gray-50 border rounded-md p-3 w-1/12'>Execute</button>
         <button className='bg-slate-950 text-gray-50  p-3 w-1/12' onClick={handleDelete}><Delete/></button>
         
       </div>
@@ -63,12 +108,18 @@ const Query = () => {
 
 
         <div className='flex flex-col w-[400px]'>
-        <p className='max-w-[400px] font-semibold'>All illegal site's servers were seized in a recent operation. Please submit all the users' details</p>
-        <div className='mt-7 flex flex-wrap space-x-4 my-2'>
+        <p className='max-w-[400px] font-semibold'>{brief}</p>
+        <div className='mt-5 flex flex-wrap space-x-4 my-2'>
         {symbols.map(symbol => <buttton 
         name={symbol} 
         onClick={() => handleClick(symbol)}
         className="bg-slate-950 text-gray-50 border rounded-md p-3 my-3">{symbol}</buttton>)}
+        </div>
+        <div className='flex flex-wrap space-x-1 my-2'>
+        {numbers.map(number => <buttton 
+        name={number} 
+        onClick={() => handleClick(number)}
+        className="bg-slate-950 text-gray-50 border rounded-md p-2 my-3">{number}</buttton>)}
         </div>
         </div>
       </div>
